7c21c596eeab26d6b4ef59f852ac4eff92cc4f0c
Charles Edward Gagnon <charles@Charless-MacBook-Pro.local>
2025-11-05 22:05:49 -0500

    moving things around

--- plutus/src/flat.rs
+++ plutus/src/flat.rs
@@ -1,107 +1,175 @@
-use std::{ffi::c_ulong, io::{Error, ErrorKind, Read, Result, Write}};
+// Non obvious thing from the spec:
+// - The msb (most significant bit) is read first in the bit stream. For exmaple, If I have 9 bits
+// to encode (0b011011011), they are encoded as such in memory: `01101101` `10000000`.
 
-use rug::Integer;
+use std::{
+    ffi::c_ulong,
+    io::{Read, Result, Write},
+    num::NonZeroU8, ops::{Deref, DerefMut},
+};
+
+use rug::{Complete, Integer};
 
 trait Encode {
-    fn encode<W: Write>(&self, writer: &mut W) -> Result<()>;
+    fn encode(&self, buffer: &mut Buffer);
 }
 
 trait Decode: Sized {
     fn decode<R: Read>(reader: &mut R) -> Result<Self>;
 }
 
-impl Encode for Integer {
-    fn encode<W: Write>(&self, writer: &mut W) -> Result<()> {
-        todo!()
+struct CleanBuffer<'a> {
+    buf: &'a mut Buffer,
+}
+
+impl<'a> Deref for CleanBuffer<'a> {
+    type Target = Buffer;
+
+    fn deref(&self) -> &Self::Target {
+        self.buf
     }
 }
 
-impl Decode for Integer {
-    fn decode<R: Read>(reader: &mut R) -> Result<Self> {
-        todo!()
+impl<'a> DerefMut for CleanBuffer<'a> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.buf
     }
 }
 
-impl Encode for [c_ulong] {
-    /// LEB128 encoding of the words. The provided words are in little endian order.
-    fn encode<W: Write>(&self, writer: &mut W) -> Result<()> {
-        let mut count = 0;
-        let mut word = 0;
-        let mut to_read = 0;
-        while to_read != 7 || count != self.len() {
-            let mut byte = (word & 0x7F) as u8;
-            word >>= 7;
-            
-            if to_read <= 7 {
-                word = self[count];
-                count += 1;
-                let shift = 7 - to_read;
-                byte <<= shift;
-                byte |= word as u8 & ((1 << shift) - 1);
-                word >>= shift;
-                to_read = if count != self.len() {
-                    c_ulong::BITS - shift
-                } else {
-                    c_ulong::BITS - word.leading_zeros()
-                };
-            }
+impl<'a> CleanBuffer<'a> {
+    fn write_bytes(&mut self, bytes: &[u8]) {
+        self.buf.buf.extend(bytes)
+    }
+}
 
-            let not_last_byte = (to_read != 0 || count != self.len()) as u8;
-            writer.write_all(&[byte | (not_last_byte) << 7])?;
+struct Buffer {
+    buf: Vec<u8>,
+    partial: u64,
+    remaining: NonZeroU8,
+}
+
+impl Buffer {
+    fn with_pad(&mut self, f: impl FnOnce(CleanBuffer<'_>)) {
+        let shift_amount = (u8::from(self.remaining) + 7) % 8 + 1;
+        self.partial <<= shift_amount;
+        self.partial |= 1;
+        let skip: usize = u8::from(self.remaining) as usize / 8;
+        self.buf.extend(&self.partial.to_be_bytes()[skip..]);
+        f(CleanBuffer {
+            buf: self,
+        })
+    }
+    
+    /// The contents provided are in the lower part of the `contents`. The msb is the
+    /// first "element", and the least significant bit is the last.
+    ///
+    /// Panics if count > 64.
+    fn write_bits(&mut self, contents: u64, count: u8) {
+        let remaining: u8 = self.remaining.into();
+        if count >= remaining {
+            self.partial <<= remaining;
+            self.partial |= contents >> (count - remaining);
+            self.buf.extend(&self.partial.to_be_bytes());
+            self.partial = contents & ((1 << (count - remaining)) - 1);
+            self.remaining = NonZeroU8::new(u64::BITS as u8 + remaining - count)
+                .expect("count should be less than 64.");
+        } else {
+            self.partial <<= count;
+            self.partial |= contents;
+            self.remaining =
+                NonZeroU8::new(remaining - count).expect("count is less than remaining");
         }
-        
-        Ok(())
     }
+
 }
 
-#[test]
-pub fn test_thing() {
-    let mut buf: Vec<u8> = Vec::new();
-    Encode::encode([0x7Fu64].as_slice(), &mut buf).unwrap();
-    dbg!(&buf);
-    Encode::encode([0x80u64].as_slice(), &mut buf).unwrap();
-    dbg!(&buf);
+/// LEB128 encoding of the words. The provided words are in little endian order.
+// Wierd Cases:
+// - The last word may not finish on a multiple of 7
+// - The last word may not have enough bytes to fulfill even one read from the previous to
+//   last word.
+// - The slice may be empty.
+fn leb128<const DOUBLE: bool>(data: &[c_ulong], buffer: &mut Buffer) {
+    let mut count = 0;
+    let mut word = 0;
+    let mut to_read = DOUBLE as u32; // If we want to double, read 1 `0` bit as lsb, then words.
+    while count != data.len() || to_read != 0 {
+        let mut byte = word & 0x7F;
+        word >>= 7;
+
+        if to_read <= 7 {
+            word = data.get(count).copied().unwrap_or(0);
+            count += 1;
+            let mut shift = 7 - to_read;
+            let old_to_read = to_read;
+            to_read = if count == data.len() {
+                let remaining = c_ulong::BITS - word.leading_zeros();
+                if remaining <= shift {
+                    shift = remaining;
+                    0
+                } else {
+                    remaining - shift
+                }
+            } else {
+                c_ulong::BITS - shift
+            };
+
+            byte |= (word & ((1 << shift) - 1)) << old_to_read;
+            word >>= shift;
+        } else {
+            to_read -= 7;
+        }
+
+        let not_last_byte = (count != data.len() || to_read != 0) as u64;
+        buffer.write_bits(byte | (not_last_byte) << 7, 8);
+    }
 }
 
 impl Encode for [u8] {
-    fn encode<W: Write>(&self, writer: &mut W) -> Result<()> {
-        self.chunks(255).try_for_each(|chunk| -> Result<()> {
-            let len = chunk.len() as u8;
-            writer.write_all(&[len])?;
-            writer.write_all(chunk)?;
-            Ok(())
-        })?;
-        writer.write_all(&[0])
-    }
-}
-
-impl Decode for Vec<u8> {
-    fn decode<R: Read>(reader: &mut R) -> Result<Self> {
-        let mut result = Vec::new();
-        loop {
-            let mut len_buf = [0u8; 1];
-            reader.read_exact(&mut len_buf)?;
-            let len = len_buf[0] as usize;
-            if len == 0 {
-                break;
-            }
-            let mut chunk = vec![0u8; len];
-            reader.read_exact(&mut chunk)?;
-            result.extend_from_slice(&chunk);
-        }
-        Ok(result)
+    fn encode(&self, buffer: &mut Buffer) {
+        buffer.with_pad(|mut buf| {
+            self.chunks(255).for_each(|chunk| {
+                let len = chunk.len() as u8;
+                buf.write_bytes(&[len]);
+                buf.write_bytes(chunk);
+            });
+            buf.write_bytes(&[0]);
+        });
     }
 }
 
 impl Encode for str {
-    fn encode<W: Write>(&self, writer: &mut W) -> Result<()> {
-        self.as_bytes().encode(writer)
+    fn encode(&self, buffer: &mut Buffer) {
+        self.as_bytes().encode(buffer)
     }
 }
 
-impl Decode for String {
-    fn decode<R: Read>(reader: &mut R) -> Result<Self> {
-        let bytes = Vec::<u8>::decode(reader)?;
-        String::from_utf8(bytes).map_err(|e| Error::new(ErrorKind::InvalidData, e))
+impl Encode for Integer {
+    fn encode(&self, buffer: &mut Buffer) {
+        if self.is_negative() {
+            let x: Integer = -(self * 2usize).complete();
+            leb128::<false>((x - 1usize).as_limbs(), buffer)
+        } else {
+            leb128::<true>(self.as_limbs(), buffer)
+        }
     }
 }
+
+
+impl Decode for Integer {
+    fn decode<R: Read>(reader: &mut R) -> Result<Self> {
+        let mut integer = Integer::new();
+        #[allow(clippy::unbuffered_bytes)]
+        for byte in reader.bytes() {
+            let byte = byte?;
+            integer |= byte & 0x7F;
+            if byte & 0x80 == 0 {
+                break;
+            }
+            integer <<= 7;
+        }
+
+        Ok(integer)
+    }
+}
+
