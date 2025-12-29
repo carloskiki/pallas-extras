ca57be3fd88f6930b5efde1f045a65c5d26443d6
Charles Edward Gagnon <76854355+carloskiki@users.noreply.github.com>
2025-12-29 09:44:25 -0500

    clear out everything but byron

--- ledger/src/lib.rs
+++ ledger/src/lib.rs
@@ -2,27 +2,7 @@
 //!
 //! All types serialize into the Babbage era of the specification, and can be deserialized from any
 //! era between and including Shelley and Babbage.
-pub mod address;
-pub mod asset;
-pub mod block;
-pub mod certificate;
-pub mod credential;
 pub mod crypto;
-pub mod pool;
-pub mod protocol;
-pub mod script;
-pub mod transaction;
-pub mod witness;
-pub mod governance;
-pub mod epoch;
 pub mod slot;
 
-pub use asset::Asset;
-pub use block::Block;
-pub use certificate::Certificate;
-pub use credential::Credential;
-pub use script::Script;
-pub use transaction::Transaction;
-
 pub mod byron;
-pub mod shelley;
--- ledger/src/script/plutus.rs
+++
@@ -1,5 +0,0 @@
-use minicbor::{CborLen, Decode, Encode};
-
-#[derive(Debug, Clone, PartialEq, Eq, Ord, PartialOrd, Hash, Encode, Decode, CborLen)]
-#[cbor(transparent)]
-pub struct Script(#[cbor(with = "minicbor::bytes")] Box<[u8]>);
