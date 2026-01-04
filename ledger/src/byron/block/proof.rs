ea5289c6873948f0d622abf406bbcbcfedbbe6a1
Charles Edward Gagnon <76854355+carloskiki@users.noreply.github.com>
2026-01-03 21:09:35 -0500

    stuff

--- ledger/src/byron/block/proof.rs
+++ ledger/src/byron/block/proof.rs
@@ -1,4 +1,10 @@
 use tinycbor_derive::{CborLen, Decode, Encode};
 
+use crate::{byron::transaction, crypto};
+
 #[derive(Debug, Clone, PartialEq, Eq, Encode, Decode, CborLen)]
-pub struct Proof {}
+pub struct Proof {
+    transaction_proof: transaction::Proof,
+    delegation_proof: crypto::Blake2b256Digest,
+    update_proof: crypto::Blake2b256Digest,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,60 @@
-// use std::{error::Error, fs::File, io::Read};
-// 
-// use const_hex::FromHex;
-// use indicatif::ProgressIterator;
-// use minicbor::{CborLen as _, Decoder, Encode, Encoder};
+use std::{
+    error::Error,
+    ffi::{OsStr, OsString},
+    fs::File,
+    io::Read,
+    os::unix::ffi::OsStrExt,
+};
 
-const SKIPS: &[u16] = &[7779];
+use tinycbor::{Decode, Decoder};
 
-// First byte is version, then all quadruples are offsets
-// Offsets in primary are u32s
-//
-// Primary File profile:
-// Secondary offsets = 56
-// slots per chunk = 21602
-// Version = 1
-//
-//
-// Secondary file layout
-// Should be 56 bytes
-// Block offset: u64 BE - 8
-// Header offset: u16 BE - 2
-// Header size: u16 BE - 2
-// CRC: to figure u32 BE - 4
-// Header hash: 32 bytes
-// block or EBB: u64 BE (either slot or epoch number, depending if EBB) - 8
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut buffer: Vec<u8> = Vec::new();
 
-fn main() {
-    println!("Hello, world!");
+    let mut files_ordered = std::fs::read_dir(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/snapshots/mainnet/immutable"
+    ))?
+    .filter_map(|res| {
+        res.ok()
+            .and_then(|f| (f.path().extension() == Some(OsStr::from_bytes(b"chunk"))).then_some(f))
+    })
+    .collect::<Vec<_>>();
+    files_ordered.sort_by_key(|dir_entry| dir_entry.file_name());
+    println!("Found {} files", files_ordered.len());
+
+    for file in files_ordered {
+        let file_name_os_str = file.file_name();
+        let file_name = file_name_os_str.to_str().ok_or("invalid file name")?;
+        if !file_name.ends_with(".chunk") {
+            continue;
+        }
+
+        let mut file = File::open(file.path())?;
+        let mut start = [0u8; 2];
+        file.read_exact(&mut start)?;
+
+        if start[1] > 1 {
+            println!("Shelly era: {file_name}");
+            continue;
+        }
+        
+        // file.read_to_end(&mut buffer)?;
+        // let mut decoder = Decoder(&buffer);
+        // loop {
+        //     if decoder.0.is_empty() {
+        //         break;
+        //     };
+
+        //     match ledger::byron::Block::decode(&mut decoder) {
+        //         Ok(_) => {
+        //             println!("Decoded block");
+        //         }
+        //         Err(e) => panic!("{e}"),
+        //     };
+        // }
+        // 
+        // buffer.clear();
+    }
+    Ok(())
 }
-
-// fn main() -> Result<(), Box<dyn Error>> {
-//     let mut buffer = Vec::new();
-//     let expected = 56;
-//     let offsets_expected = 21602;
-// 
-//     for file in std::fs::read_dir(concat!(
-//         env!("CARGO_MANIFEST_DIR"),
-//         "/snapshots/preprod/immutable"
-//     ))? {
-//         let file = file?;
-//         let os_file_name = file.file_name();
-//         let file_name = os_file_name.to_str().unwrap();
-//         let path = file.path();
-//         if !file_name.ends_with(".primary") {
-//             continue
-//         }
-// 
-//         let mut file = std::fs::File::open(path)?;
-//         file.read_to_end(&mut buffer)?;
-//         let offsets = buffer[1..]
-//             .chunks(4)
-//             .map(|bytes| {
-//                 u32::from_be_bytes(bytes.try_into().unwrap())
-//             })
-//             .collect::<Vec<_>>();
-//         if offsets_expected != offsets.len() {
-//             println!("things: {}", file_name);
-//         }
-//         
-//         let gap = offsets.windows(2).all(|slice| {
-//             slice[0] == slice[1] || slice[1] - slice[0] == expected
-//         });
-//         assert!(gap);
-//         buffer.clear()
-//     }
-// 
-//     
-//     Ok(())
-// }
-// 
-// fn empty_asset<T>(asset: &ledger::Asset<T>) -> bool {
-//     asset.0.is_empty() || asset.0.iter().any(|(_, bundles)| bundles.0.is_empty())
-// }
-// 
-// // Basically eras + 1, because the tag 0 is Byron EBB block
-// fn db_block_thing(
-//     d: &mut minicbor::Decoder<'_>,
-// ) -> Result<Option<(ledger::Block, u8)>, minicbor::decode::Error> {
-//     cbor_util::array_decode(
-//         2,
-//         |d| {
-//             let tag = d.u8()?;
-//             if tag < 2 {
-//                 return Ok(None);
-//             }
-//             Ok(Some((d.decode()?, tag)))
-//         },
-//         d,
-//     )
-// }
-// 
-// fn inspect_tokens(cbor: &[u8]) {
-//     for token in Decoder::new(cbor).tokens() {
-//         let token = token.unwrap();
-//         println!("{}", token);
-//     }
-// }
-// 
-// // Conway first block: 2344009
