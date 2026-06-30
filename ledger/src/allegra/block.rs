330165c923ece13c4bcf5c78d310f7f7250fa179
Charles Edward Gagnon <76854355+carloskiki@users.noreply.github.com>
2026-06-30 09:50:32 -0400

    don't use `With` for block members

--- ledger/src/allegra/block.rs
+++ ledger/src/allegra/block.rs
@@ -7,7 +7,7 @@ pub use header::Header;
 
 #[derive(Debug, Clone, PartialEq, Eq, Encode, Decode, CborLen)]
 pub struct Block<'a> {
-    pub header: With<'a, Header<'a>>,
+    pub header: Header<'a>,
     pub transaction_bodies: Vec<super::transaction::Body<'a>>,
     pub transaction_witness_sets: Vec<allegra::transaction::witness::Set<'a>>,
     pub transaction_data: crate::Unique<Vec<(Index, allegra::transaction::Data<'a>)>, false>,
